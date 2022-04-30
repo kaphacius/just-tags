efefdb72acbb07871b92b7e8aafb0185f2d81b99
Yurii Zadoianchuk <yurii.zadoianchuk@gmail.com>
2022-04-30 12:43:25 +0200

    Improve keyboard shortcuts for opening main and diff view

--- EMVTags/EMVTagsApp.swift
+++ EMVTags/EMVTagsApp.swift
@@ -18,57 +18,75 @@ internal struct EMVTagsApp: App {
         WindowGroup {
             MainView()
                 .environmentObject(infoDataSource)
-                .handlesExternalEvents(preferring: Set(arrayLiteral: "main"), allowing: Set(arrayLiteral: "*"))
+                .handlesExternalEvents(preferring: ["main"], allowing: ["main"])
         }
-        .handlesExternalEvents(matching: Set(arrayLiteral: "main"))
-        .commands {
-            CommandGroup(before: CommandGroupPlacement.newItem, addition: {
-                Button(action: {
-                    if let currentWindow = NSApp.keyWindow,
-                       let windowController = currentWindow.windowController {
-                        windowController.newWindowForTab(nil)
-                        if let newWindow = NSApp.keyWindow, currentWindow != newWindow {
-                            currentWindow.addTabbedWindow(newWindow, ordered: .above)
-                        }
-                    }
-                }, label: {
-                    Text("New Tab")
-                }).keyboardShortcut("t", modifiers: [.command])
-                Button(action: {
-                    let openPanel = NSOpenPanel()
-                    openPanel.canChooseDirectories = true
-                    openPanel.canChooseFiles = true
-                    openPanel.allowedContentTypes = [.json]
-                    guard openPanel.runModal() == .OK else { return }
-                    
-                    let data = try! Data(contentsOf: openPanel.url!)
-                    
-                    let result = try! JSONDecoder().decode(TagInfoContainer.self, from: data)
-                    
-                    print(result.tags.count)
-                    
-                    infoDataSource.infoList.append(contentsOf: result.tags)
-                }, label: {
-                    Text("New Tab")
-                }).keyboardShortcut("o", modifiers: [.command, .shift])
-                Button(action: {
-                    openURL(URL(string: "emvtags://diff")!)
-                }, label: {
-                    Text("Diff view")
-                }).keyboardShortcut("d", modifiers: [.command, .shift])
-                Button(action: {
-                    openURL(URL(string: "emvtags://main")!)
-                }, label: {
-                    Text("Main view")
-                }).keyboardShortcut("m", modifiers: [.command, .shift])
-            })
-        }
+        .handlesExternalEvents(matching: ["main"])
+        .commands(content: commands)
         
         WindowGroup("Diff") {
             DiffView()
                 .environmentObject(infoDataSource)
-                .handlesExternalEvents(preferring: Set(arrayLiteral: "diff"), allowing: Set(arrayLiteral: "*"))
-        }.handlesExternalEvents(matching: Set(arrayLiteral: "diff"))
+                .handlesExternalEvents(preferring: ["diff"], allowing: ["diff"])
+        }.handlesExternalEvents(matching: ["diff"])
+    }
+    
+    private func commands() -> some Commands {
+        CommandGroup(before: CommandGroupPlacement.newItem, addition: {
+            newTabButton
+            openTagInfoButton
+            openMainViewButton
+            openTagInfoButton
+        })
+    }
+    
+    private var newTabButton: some View {
+        Button(action: {
+            if let currentWindow = NSApp.keyWindow,
+               let windowController = currentWindow.windowController {
+                windowController.newWindowForTab(nil)
+                if let newWindow = NSApp.keyWindow, currentWindow != newWindow {
+                    currentWindow.addTabbedWindow(newWindow, ordered: .above)
+                }
+            }
+        }, label: {
+            Text("New Tab")
+        }).keyboardShortcut("t", modifiers: [.command])
+    }
+    
+    private var openTagInfoButton: some View {
+        Button(action: {
+            let openPanel = NSOpenPanel()
+            openPanel.canChooseDirectories = true
+            openPanel.canChooseFiles = true
+            openPanel.allowedContentTypes = [.json]
+            guard openPanel.runModal() == .OK else { return }
+            
+            let data = try! Data(contentsOf: openPanel.url!)
+            
+            let result = try! JSONDecoder().decode(TagInfoContainer.self, from: data)
+            
+            print(result.tags.count)
+            
+            infoDataSource.infoList.append(contentsOf: result.tags)
+        }, label: {
+            Text("New Tab")
+        }).keyboardShortcut("o", modifiers: [.command, .shift])
+    }
+    
+    private var openDiffViewButton: some View {
+        Button(action: {
+            openURL(URL(string: "emvtags://diff")!)
+        }, label: {
+            Text("Diff view")
+        }).keyboardShortcut("d", modifiers: [.command, .shift])
+    }
+    
+    private var openMainViewButton: some View {
+        Button(action: {
+            openURL(URL(string: "emvtags://main")!)
+        }, label: {
+            Text("Main view")
+        }).keyboardShortcut("m", modifiers: [.command, .shift])
     }
     
     internal init() {
