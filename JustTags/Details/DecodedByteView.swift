23901165e0d18317dc994f1e9aadeb10cbdf1eed
Yurii <yurii.zadoianchuk@gmail.com>
2022-10-10 21:03:29 +0200

    Add DecodedRowView

---
+++ JustTags/Details/DecodedByteView.swift
@@ -0,0 +1,20 @@
+//
+//  DecodedByteView.swift
+//  JustTags
+//
+//  Created by Yurii Zadoianchuk on 10/10/2022.
+//
+
+import SwiftUI
+
+struct DecodedByteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DecodedByteView_Previews: PreviewProvider {
+    static var previews: some View {
+        DecodedByteView()
+    }
+}
