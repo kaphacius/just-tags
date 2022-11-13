7065c7c47f2299714c42ef119da136b3ae4d59b0
Yurii <yurii.zadoianchuk@gmail.com>
2022-11-13 07:57:35 +0100

    Remove unused code

--- JustTags/Settings/Resources/TagMapping/TagMappingView.swift
+++ JustTags/Settings/Resources/TagMapping/TagMappingView.swift
@@ -5,4 +5,22 @@
 //  Created by Yurii Zadoianchuk on 12/11/2022.
 //
 
-import Foundation
+import SwiftUI
+import SwiftyEMVTags
+
+struct TagMappingView: CustomResourceView {
+    
+    typealias Resource = TagMapping
+    
+    let resource: TagMapping
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: commonPadding) {
+                TagValueView(
+                    vm: .init(value: resource.tag.hexString, extendedDescription: nil)
+                )
+            }
+        }
+    }
+}
