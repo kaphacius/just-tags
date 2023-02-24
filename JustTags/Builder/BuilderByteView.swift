58e60546438139ce8e60665bf1661ae698fc519c
Yurii <yurii.zadoianchuk@gmail.com>
2023-02-24 18:06:42 +0100

    Add BuilderByteView

---
+++ JustTags/Builder/BuilderByteView.swift
@@ -0,0 +1,88 @@
+//
+//  BuilderByteView.swift
+//  JustTags
+//
+//  Created by Yurii Zadoianchuk on 24/02/2023.
+//
+
+import SwiftUI
+import SwiftyBERTLV
+
+internal struct BuilderByteView: View {
+    
+    @State var byte: UInt8 = 0xAB
+
+    var body: some View {
+        let byteBits = byte.bits
+        let byteHexDigits = byte.hexDigits
+        
+        HStack {
+            nibble(
+                nibbleBits: Array(byteBits[0..<4]),
+                hexDigit: byteHexDigits[0],
+                startIdx: 0
+            )
+            nibble(
+                nibbleBits: Array(byteBits[4..<8]),
+                hexDigit: byteHexDigits[1],
+                startIdx: 4
+            )
+        }.padding(commonPadding * 2)
+    }
+    
+    @ViewBuilder
+    private func bits(_ bits: [UInt8], startIdx: Int) -> some View {
+        HStack {
+            ForEach(Array(bits.enumerated()), id: \.offset) { (offset, bit) in
+                Button(action: {
+                    onBitFlip(idx: startIdx + offset)
+                }) {
+                    Text(bit == 1 ? "1" : "0")
+                        .font(.body.monospaced())
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func nibble(nibbleBits: [UInt8], hexDigit: String, startIdx: Int) -> some View {
+        GroupBox {
+            VStack {
+                bits(nibbleBits, startIdx: startIdx)
+                Text(hexDigit)
+                    .font(.title.monospaced())
+            }
+        }
+    }
+    
+    private func onBitFlip(idx: Int) {
+        self.byte = self.byte.flippingBit(at: idx)
+    }
+    
+}
+
+extension UInt8 {
+    
+    var bits: [UInt8] {
+        (0..<Self.bitWidth).map { idx in
+            (self >> idx) & 0x01
+        }.reversed()
+    }
+    
+    var hexDigits: [String] {
+        self.hexString.split(by: 1)
+    }
+
+    func flippingBit(at idx: Int) -> UInt8 {
+        let tidx = Self.bitWidth - 1 - idx
+        let mask: UInt8 = 0x01 << tidx
+        return self ^ mask
+    }
+    
+}
+
+struct BuilderByteView_Previews: PreviewProvider {
+    static var previews: some View {
+        BuilderByteView()
+    }
+}
